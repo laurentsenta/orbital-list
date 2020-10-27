01b7b4f21eb4922b272bd20723fa01c1548cd2a3
Laurent Senta <laurent.senta@gmail.com>
2020-10-27 11:26:32 +0100

    wip

--- src/TimezoneClock.tsx
+++ src/TimezoneClock.tsx
@@ -6,6 +6,31 @@ import { toDeg, useDatetime, useEasedState } from './utils'
 
 interface IProps {
   items: Item[]
+  setCurrentTime?: (x: Date) => void
+}
+
+function useDebounce<T>(value: T, delay: number = 500) {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value)
+      }, delay)
+
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler)
+      }
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  )
+
+  return debouncedValue
 }
 
 const TimezoneClock = (props: IProps) => {
@@ -49,6 +74,12 @@ const TimezoneClock = (props: IProps) => {
 
   const time = new Date(myTime)
   time.setMinutes(time.getMinutes() + delta)
+
+  const debouncedTime = useDebounce(time, 1500)
+
+  useEffect(() => {
+    props.setCurrentTime ? props.setCurrentTime(debouncedTime) : null
+  }, [debouncedTime, props.setCurrentTime])
 
   const items = augmentItems(time, props.items)
 
